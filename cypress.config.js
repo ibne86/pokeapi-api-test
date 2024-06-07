a10a393495bb9bddb43e948f787a6b5f63ddd05f
ibne86 <khanibne@gmail.com>
2024-06-07 14:52:07 +0200

    adding base url and setting up to run specs file

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://pokeapi.co',
+    specPattern: "cypress/e2e/*.spec.{js,jsx,ts,tsx}"
   },
 });
