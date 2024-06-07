413d1da861b92c50c57ca6817c00a413ff4c53b7
ibne86 <khanibne@gmail.com>
2024-06-07 16:03:10 +0200

    adding test script

---
+++ cypress/e2e/berryName.spec.js
@@ -0,0 +1,20 @@
+describe("Berry GET Request Test wtih Name", () => {
+  beforeEach(() => {
+    cy.fixture("berryNames").as("berryNames");
+  });
+
+  it("Validate GET request with valid name", function () {
+    cy.getBerry(this.berryNames.validBerryName, false).as("response");
+      cy.get("@response").then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body.firmness).to.have.property('name', 'super-hard');
+    });
+  });
+
+  it("Validate GET request with invalid name", function () {
+    cy.getBerry(this.berryNames.invalidBerryName, false).then((response) => {
+      expect(response.status).to.eq(404);
+      expect(response.body).to.contain("Not Found");
+    });
+  });
+});
