17e9ba3dd191b264c84499f28a4396f24756526f
Ibne Sina Khan <ibne.khan@smartbear.com>
2024-06-09 21:53:50 +0200

    Refactor code for consistency (#6)
    * refactoring berryId spec for consistency
    
    * refactoring berryName spec for consistency
    
    ---------
    
    Co-authored-by: ibne86 <khanibne@gmail.com>

--- cypress/e2e/berryId.spec.js
+++ cypress/e2e/berryId.spec.js
@@ -4,15 +4,17 @@ describe('Berry GET Request Test wtih Id', () => {
   });
 
   it('Validate GET request with valid Id', function() {
-    cy.getBerry(this.berryIds.validBerryId).then((response) => {
-      expect(response.status).to.eq(200);
+    cy.getBerry(this.berryIds.validBerryId).as("response");
+    cy.get("@response").then((response) => {
+      expect(response.status).to.equal(200);
       expect(response.body.firmness).to.have.property('name', 'soft');
     });
   });
 
   it('Validate  GET request with invalid Id', function() {
-    cy.getBerry(this.berryIds.invalidBerryId,false).then((response) => {
-      expect(response.status).to.eq(404);
+    cy.getBerry(this.berryIds.invalidBerryId,false).as("response");
+    cy.get("@response").then((response) => {
+      expect(response.status).to.equal(404);
       expect(response.body).to.contain("Not Found");
     });
   });
