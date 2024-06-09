cbc81cc2638a7fe59c2188277f51fda3845c0f31
ibne86 <khanibne@gmail.com>
2024-06-09 21:53:23 +0200

    refactoring berryName spec for consistency

--- cypress/e2e/berryName.spec.js
+++ cypress/e2e/berryName.spec.js
@@ -1,19 +1,21 @@
 describe("Berry GET Request Test wtih Name", () => {
   beforeEach(() => {
     cy.fixture("berryNames").as("berryNames");
+
   });
 
   it("Validate GET request with valid name", function () {
     cy.getBerry(this.berryNames.validBerryName).as("response");
       cy.get("@response").then((response) => {
-        expect(response.status).to.equal(200);
-        expect(response.body.firmness).to.have.property('name', 'super-hard');
+      expect(response.status).to.equal(200);        
+      expect(response.body.firmness).to.have.property('name', 'super-hard');
     });
   });
 
   it("Validate GET request with invalid name", function () {
-    cy.getBerry(this.berryNames.invalidBerryName, false).then((response) => {
-      expect(response.status).to.eq(404);
+    cy.getBerry(this.berryNames.invalidBerryName, false).as("response");
+    cy.get("@response").then((response) => {
+      expect(response.status).to.equal(404);
       expect(response.body).to.contain("Not Found");
     });
   });
